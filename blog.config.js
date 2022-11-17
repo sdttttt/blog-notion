64000ddc4fb6dcb47416740c4c9b78353d120be5
Kii <Kaltsit@111.com>
2022-11-17 12:48:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 	APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
 	// 自定义字体示例： 请先将 CUSTOM_FONT 改为 true， 并将 CUSTOM_FONT_URL 改为你的字体CSS地址，同时在 CUSTOM_FONT_SANS 与 CUSTOM_FONT_SERIF 中指定你的 fontfamily
-	CUSTOM_FONT: false, // 是否使用自定义字体
+	CUSTOM_FONT: true, // 是否使用自定义字体
 	CUSTOM_FONT_URL: ['https://cdn.jsdelivr.net/npm/lxgw-wenkai-screen-webfont@1.6.0/lxgwwenkaiscreen.css'], // 自定义字体的CSS
 	CUSTOM_FONT_SANS: ['LXGW WenKai Screen'], // 自定义无衬线字体
 	CUSTOM_FONT_SERIF: ['LXGW WenKai Screen'], // 自定义衬线字体
